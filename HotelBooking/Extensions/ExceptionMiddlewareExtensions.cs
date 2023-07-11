680886c0b510f11ed241943999321d78c8e9296d
alexander valdaitsevv <valdaitsevv@mail.ru>
2023-07-11 20:30:11 +0300

    refactor: exception middleware sends dynamic StatusCodes and Messages

--- HotelBooking/Extensions/ExceptionMiddlewareExtensions.cs
+++ HotelBooking/Extensions/ExceptionMiddlewareExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net;
 using Contracts;
 using Entities.ErrorModel;
+using Entities.Exceptions;
 using Microsoft.AspNetCore.Diagnostics;
 namespace HotelBooking.Extensions;
 
@@ -18,13 +19,19 @@ public static class ExceptionMiddlewareExtensions
                 var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
                 if (contextFeature != null)
                 {
+                    context.Response.StatusCode = contextFeature.Error switch
+                    {
+                        NotFoundException => StatusCodes.Status404NotFound,
+                        _ => StatusCodes.Status500InternalServerError
+                    };
+
                     logger.LogError($"Something went wrong: {contextFeature.Error}");
 
                     await context.Response.WriteAsync(
                         new ErrorDetails()
                         {
                             StatusCode = context.Response.StatusCode,
-                            Message = "Internal Server Error."
+                            Message = contextFeature.Error.Message
                         }.ToString()
                     );
                 }
