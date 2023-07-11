9ede0574b4061cde416e69631458a42bdbc2ea3e
alexander valdaitsevv <valdaitsevv@mail.ru>
2023-07-12 01:49:20 +0300

    fix: 500 status code when HotelNotFound exception

--- Entities/Exceptions/HotelNotFoundException.cs
+++ Entities/Exceptions/HotelNotFoundException.cs
@@ -1,6 +1,6 @@
 ﻿namespace Entities.Exceptions;
 
-public class HotelNotFoundException : Exception
+public class HotelNotFoundException : NotFoundException
 {
     public HotelNotFoundException(Guid hotelId)
         : base($"The hotel with id: {hotelId} doesn't exist in the database.")
