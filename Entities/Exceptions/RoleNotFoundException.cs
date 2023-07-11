413d52f7149f778e141ca6cd983c1ea37a28122a
alexander valdaitsevv <valdaitsevv@mail.ru>
2023-07-11 20:26:23 +0300

    feat: add base NotFoundException and child RoleNotFoundException

---
+++ Entities/Exceptions/RoleNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace Entities.Exceptions;
+
+public class RoleNotFoundException : NotFoundException
+{
+    public RoleNotFoundException(Guid roleId)
+        : base($"The role with id: {roleId} doesn't exist in the database.")
+    { }
+}
